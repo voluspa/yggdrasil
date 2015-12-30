0742414ade7ddfa754af9821430a83ac8a733d84
Matthew Philyaw <matthew.philyaw@gmail.com>
2015-12-30 15:22:26 -0500

    no need to check changeset twice.
    It will not insert into the database or call out to the database until
    all other validations pass. Good call @copenhas.

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -18,25 +18,18 @@ defmodule Yggdrasil.RegistrationController do
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
 
-    # this whole block needs to be simplified
-    # copied from various sources
-    if changeset.valid? do
-      result = changeset
-        |> put_change(:hash, hashpwsalt(changeset.params["password"]))
-        |> Repo.insert
+    result = changeset
+      |> put_change(:hash, hashpwsalt(changeset.params["password"]))
+      |> Repo.insert
 
-      # need to check after insert, username has unique constraint and only will fire after insert
-      case result do 
-        {:ok, new_user} -> 
-          conn
-            |> put_flash(:info, "Succesfully registered and logged in")
-            |> put_session(:current_user, new_user)
-            |> redirect(to: client_path(conn, :index))
-        {:error, result_changeset} ->
-          render conn, "new.html", changeset: result_changeset
-      end
-    else
-      render conn, "new.html", changeset: changeset
+    case result do 
+      {:ok, new_user} -> 
+        conn
+          |> put_flash(:info, "Succesfully registered and logged in")
+          |> put_session(:current_user, new_user)
+          |> redirect(to: client_path(conn, :index))
+      {:error, result_changeset} ->
+        render conn, "new.html", changeset: result_changeset
     end
   end
 end
