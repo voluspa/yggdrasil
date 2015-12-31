1ac37bc9ada9d044d15ff6e9144c97728ec19242
Matthew Philyaw <matthew.philyaw@gmail.com>
2015-12-31 10:40:58 -0500

    add tests to verify hash is generated on valid changesets and not when invalid.

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -2,6 +2,7 @@ defmodule Yggdrasil.UserTest do
   use Yggdrasil.ModelCase
 
   alias Yggdrasil.User
+  alias Comeonin.Bcrypt
 
   @min_len 4
   @password "password"
@@ -23,6 +24,17 @@ defmodule Yggdrasil.UserTest do
   @unique_msg "has already been taken"
   @invalid_length_msg "should be at least %{count} character(s)"
 
+  # -- helpers
+
+  defp valid_hash(hash) do
+    Bcrypt.checkpw(@password, hash)
+  end
+
+  defp hash_missing?(changeset) do
+    !Map.has_key?(changeset.changes, :hash)
+  end
+
+  # -- tests
 
   test "create_changeset with valid attributes" do
     changeset = User.create_changeset(%User{}, @valid_attrs)
@@ -82,9 +94,69 @@ defmodule Yggdrasil.UserTest do
     refute changeset.valid?
   end
 
-  test "changset downcases username" do
+  test "create_changeset downcases username" do
     user = User.create_changeset(%User{}, @valid_attrs_upcase_user)
 
     assert user.changes[:username] == "tester"
+  end
+
+  test "create_changet genreates hash if valid" do
+    changeset = User.create_changeset(%User{}, @valid_attrs)
+
+    assert Map.has_key?(changeset.changes, :hash)
+
+    hash = changeset.changes[:hash]
+    assert valid_hash(hash)
+  end
+
+  test "create_changet doesn't genreates hash if username missing" do
+    changeset = User.create_changeset(%User{}, @valid_attrs)
+
+    assert Map.has_key?(changeset.changes, :hash)
+
+    hash = changeset.changes[:hash]
+    assert valid_hash(hash)
+  end
+
+  test "create_changeset with missing username doesn't generate hash" do
+    changeset = User.create_changeset(%User{}, @missing_username)
+
+    refute changeset.valid?
+    assert hash_missing?(changeset)
+  end
+
+  test "create_changeset with missing password doesn't generate hash" do
+    changeset = User.create_changeset(%User{}, @missing_password)
+
+    refute changeset.valid?
+    assert hash_missing?(changeset)
+  end
+
+  test "create_changeset with missing password_confirmation doesn't generate hash" do
+    changeset = User.create_changeset(%User{}, @missing_password_conf)
+
+    refute changeset.valid?
+    assert hash_missing?(changeset)
+  end
+
+  test "create_changeset with password mismatch doesn't generate hash" do
+    changeset = User.create_changeset(%User{}, @password_mismatch)
+
+    refute changeset.valid?
+    assert hash_missing?(changeset)
+  end
+
+  test "create_changeset with password less than min doesn't generate hash" do
+    changeset = User.create_changeset(%User{}, @password_bad_len)
+
+    refute changeset.valid?
+    assert hash_missing?(changeset)
+  end
+
+  test "create_changeset with password_confirmation less than min doesn't generate hash" do
+    changeset = User.create_changeset(%User{}, @password_conf_bad_len)
+
+    refute changeset.valid?
+    assert hash_missing?(changeset)
   end
 end
