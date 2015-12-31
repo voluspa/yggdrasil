6bba3c57e9d14481120920bc7fa3edfca3bc796f
Matthew Philyaw <matthew.philyaw@gmail.com>
2015-12-31 10:23:54 -0500

    refactored attributes to be cleaner and use one base map

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,22 +3,26 @@ defmodule Yggdrasil.UserTest do
 
   alias Yggdrasil.User
 
-  @valid_attrs %{username: "tester", password: "password", password_confirmation: "password"}
-  @valid_attrs_upcase_user %{username: "TESTER", password: "password", password_confirmation: "password"}
+  @min_len 4
+  @password "password"
+  @short_password String.slice(@password, 1..(@min_len - 1))
 
-  @missing_username %{password: "password", password_confirmation: "pass"}
-  @missing_password %{username: "tester", password_confirmation: "password"}
-  @missing_password_conf %{username: "tester", password: "password"}
-  @password_mismatch %{username: "tester", password: "test", password_confirmation: "tester"}
-  @password_bad_len %{username: "tester", password: "te", password_confirmation: "tester"}
-  @password_conf_bad_len %{username: "tester", password: "te", password_confirmation: "te"}
+  @valid_attrs %{username: "tester", password: @password, password_confirmation: @password }
+
+  @valid_attrs_upcase_user    Map.update!(@valid_attrs, :username, &String.upcase/1)
+  @missing_username           Map.drop(@valid_attrs, [:username])
+  @missing_password           Map.drop(@valid_attrs, [:password])
+  @missing_password_conf      Map.drop(@valid_attrs, [:password_confirmation])
+  @password_mismatch          %{@valid_attrs | :password => "not password"}
+  @password_bad_len           %{@valid_attrs | :password => @short_password}
+  @password_conf_bad_len      %{@valid_attrs | :password => @short_password,
+                                               :password_confirmation => @short_password}
 
   @blank_msg "can't be blank"
   @doesntmatch_msg "does not match confirmation"
   @unique_msg "has already been taken"
-
   @invalid_length_msg "should be at least %{count} character(s)"
-  @min_count 4
+
 
   test "create_changeset with valid attributes" do
     changeset = User.create_changeset(%User{}, @valid_attrs)
@@ -56,14 +60,14 @@ defmodule Yggdrasil.UserTest do
   test "create_changeset with password less than min fails" do
     changeset = User.create_changeset(%User{}, @password_bad_len)
 
-    assert changeset.errors[:password] == {@invalid_length_msg, [count: @min_count]}
+    assert changeset.errors[:password] == {@invalid_length_msg, [count: @min_len]}
     refute changeset.valid?
   end
 
   test "create_changeset with password_confirmation less than min fails" do
     changeset = User.create_changeset(%User{}, @password_conf_bad_len)
 
-    assert changeset.errors[:password_confirmation] == {@invalid_length_msg, [count: @min_count]}
+    assert changeset.errors[:password_confirmation] == {@invalid_length_msg, [count: @min_len]}
     refute changeset.valid?
   end
 
@@ -73,7 +77,7 @@ defmodule Yggdrasil.UserTest do
     {:ok, _user} = user |> Repo.insert
 
     # try again to invoke unique constraint
-    {error, changeset} = user |> Repo.insert
+    {:error, changeset} = user |> Repo.insert
     assert changeset.errors[:username] == @unique_msg
     refute changeset.valid?
   end
