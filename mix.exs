a5cfee023205203138461ebe221698677b0bb199
Sean Copenhaver <sean.copenhaver@gmail.com>
2016-01-02 21:39:48 -0500

    bump elixir version in mix.ex

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Yggdrasil.Mixfile do
   def project do
     [app: :yggdrasil,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
