9c293c3d8b47fba44c37f3788a761045c4d84612
Sean Copenhaver <sean.copenhaver@gmail.com>
2016-01-28 22:15:10 -0500

    tests to document that the command can fail to parse

--- test/channels/player_channel_test.exs
+++ test/channels/player_channel_test.exs
@@ -10,9 +10,7 @@ defmodule PlayerChannelTest do
   test "it will join the player to the game when recieving a 'join_game'" do
     user_id = "20"
 
-    {:ok, _reply, socket} = join_channel user_id
-    ref = push(socket, "join_game")
-    assert_reply ref, :ok
+    join_game user_id
 
     assert Yggdrasil.Player.Registry.is_online(user_id, sync: true)
   end
@@ -20,18 +18,32 @@ defmodule PlayerChannelTest do
   test "it notifies the player if another client issues a 'join_game'" do
     user_id = "20"
 
-    {:ok, _reply, socket1} = join_channel user_id
-
-    ref = push(socket1, "join_game")
-    assert_reply ref, :ok
+    join_game user_id
 
     # simulates a second client trying to join
     Task.async fn ->
       {:ok, _reply, socket2} = join_channel user_id
-      push(socket2, "join_game")
+      push socket2, "join_game" 
     end
 
     assert_push "event", %Message{message: "Another client has tried to connect"}
+  end
+
+  test "it accepts commands as a 'player_cmd' event"  do
+    user_id = "20"
+
+    socket = join_game user_id
+
+    push socket, "player_cmd", %{ "text" => "move east" }
+  end
+
+  test "it sends an 'error' event if the command fails to parse" do
+    user_id = "20"
+
+    socket = join_game user_id
+
+    push socket, "player_cmd", %{ "text" => "I hope this doesn't work" }
+    assert_push "event", %Message{}
   end
 
   defp join_channel(user_id, topic \\ nil)
@@ -40,5 +52,13 @@ defmodule PlayerChannelTest do
 
     socket(:nil, %{ user: user_id})
     |> subscribe_and_join(PlayerChannel, topic)
+  end
+
+  defp join_game(user_id) do
+    {:ok, _reply, socket} = join_channel user_id
+    ref = push socket, "join_game"
+    assert_reply ref, :ok
+
+    socket
   end
 end
