e0a30b35a1a04bf7ffa70b4a3f334f208177a610
Matthew Philyaw <matthew.philyaw@gmail.com>
2016-03-02 16:24:47 -0500

    refactored EnsurePermission's a little bit.

--- apps/yggdrasil_web/web/controllers/character_controller.ex
+++ apps/yggdrasil_web/web/controllers/character_controller.ex
@@ -2,8 +2,6 @@ defmodule YggdrasilWeb.CharacterController do
   use YggdrasilWeb.Web, :controller
   use Guardian.Phoenix.Controller
 
-  import YggdrasilWeb.EnsurePermission, only: [check_permissions: 2]
-
   alias Yggdrasil.Character
   alias YggdrasilWeb.EnsurePermission
 
@@ -11,13 +9,11 @@ defmodule YggdrasilWeb.CharacterController do
   plug EnsurePermission, [character: [:write]] when action in [:create, :delete]
 
   def index(conn, _params, user, _claims) do
-    user_id = user.id
-
-    query = if check_permissions(conn, character: [:admin]) do
+    query = if admin?(conn) do
       Character
     else
       from c in Character,
-      where: c.user_id == ^user_id,
+      where: c.user_id == ^user.id,
       select: c
     end
 
@@ -27,13 +23,20 @@ defmodule YggdrasilWeb.CharacterController do
   end
 
   def show(conn, %{"char_id" => char_id}, user, _claims) do
-    user_id = user.id
 
     # returns nil if not found
     # need to sort out what we want here.
-    char = Repo.one from c in Character,
-                    where: c.id == ^char_id and c.user_id == ^user_id,
-                    select: c
+    query = if admin?(conn) do
+      from c in Character,
+      where: c.id == ^char_id,
+      select: c
+    else
+      from c in Character,
+      where: c.id == ^char_id and c.user_id == ^user.id,
+      select: c
+    end
+
+    char = Repo.one query
 
     render conn, :show, data: char
   end
@@ -51,13 +54,19 @@ defmodule YggdrasilWeb.CharacterController do
   end
 
   def delete(conn, %{"char_id" => char_id}, user, _claims) do
-    user_id = user.id
+    query = if admin?(conn) do
+      from c in Character,
+      where: c.id == ^char_id,
+      select: c
+    else
+      from c in Character,
+      where: c.id == ^char_id and c.user_id == ^user.id,
+      select: c
+    end
 
     # returns nil if not found
     # need to sort out what we want here.
-    char = Repo.one from c in Character,
-                    where: c.id == ^char_id and c.user_id == ^user_id,
-                    select: c
+    char = Repo.one query
 
     do_delete conn, char
   end
@@ -76,5 +85,9 @@ defmodule YggdrasilWeb.CharacterController do
       {:error, err_changeset} ->
         render conn, :errors, data: err_changeset
     end
+  end
+
+  defp admin?(conn) do
+    EnsurePermission.all?(conn, character: [:admin])
   end
 end
