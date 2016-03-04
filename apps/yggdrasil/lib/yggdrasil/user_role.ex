abfdab41476f20895c1a4ba6655ddff5551e3943
Matthew Philyaw <matthew.philyaw@gmail.com>
2016-03-03 22:20:23 -0500

    dropping primary key for users_roles

--- apps/yggdrasil/lib/yggdrasil/user_role.ex
+++ apps/yggdrasil/lib/yggdrasil/user_role.ex
@@ -1,6 +1,7 @@
 defmodule Yggdrasil.UserRole do
   use Ecto.Schema
 
+  @primary_key false
   schema "users_roles" do
     belongs_to :user, Yggdrasil.User
     belongs_to :role, Yggdrasil.Role
--- apps/yggdrasil/priv/repo/migrations/20160303191523_create_users_roles.exs
+++ apps/yggdrasil/priv/repo/migrations/20160303191523_create_users_roles.exs
@@ -2,14 +2,13 @@ defmodule Yggdrasil.Repo.Migrations.CreateUsersRoles do
   use Ecto.Migration
 
   def change do
-    create table(:users_roles) do
+    create table(:users_roles, primary_key: false) do
       add :user_id, references(:users, on_delete: :nothing)
       add :role_id, references(:roles, on_delete: :nothing)
 
       timestamps
     end
     create index(:users_roles, [:user_id])
-    create index(:users_roles, [:role_id])
 
   end
 end
