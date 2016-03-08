9d6d9af7a8f74cfa6cd2ba65785deb69ef071d45
Matthew Philyaw <matthew.philyaw@gmail.com>
2016-03-07 23:17:29 -0500

    did something stupid! priv is sym linked in the apps _build dir so this does work.

--- apps/yggdrasil/test/test_helper.exs
+++ apps/yggdrasil/test/test_helper.exs
@@ -5,5 +5,5 @@ repo = ~w(-r Yggdrasil.Repo --quiet)
 Mix.Task.run "ecto.create", repo
 Mix.Task.run "ecto.migrate", repo
 
-run_path = Path.join([__DIR__, "..", "priv", "repo", "seeds.exs"])
+run_path = Application.app_dir :yggdrasil, Path.join(["priv", "repo", "seeds.exs"])
 Mix.Task.run "run", [run_path]
